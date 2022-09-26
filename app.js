0da790833dacd1d16bbc7ae4f3fc807426971eec
felixfenu <ext_fefenuqu@mercadolibre.com>
2022-09-26 18:20:42 -0300

    cambios app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.join(__dirname,"/public")));
-
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
 });
